e92ef59a6b804ce0047afa6cd5fc7e356a4b8db5
Mertcan Mehmetoğlu <mertcanmehmetoglu15@gmail.com>
2023-12-18 22:39:27 +0300

    mertcan 12.18

--- src/test/java/pages/mertcan/US_10_ViceDean_Management_Page.java
+++ src/test/java/pages/mertcan/US_10_ViceDean_Management_Page.java
@@ -30,27 +30,28 @@ public class US_10_ViceDean_Management_Page {
     public WebElement verifyuserName;
 
     //TC_02
-    @FindBy(xpath = "//button[.='Menu']")
-    public WebElement menuButton;
-    @FindBy(xpath = "//a[.='Lesson Management']")
-    public WebElement lessonmanagement;
-    @FindBy(xpath = "//button[.='Lesson Program']")
     public WebElement lessonprogram;
     @FindBy(xpath = "//div[@class=' css-19bb58m']")
     public WebElement chooselessonbutton;
+    @FindBy(xpath = "(//div[.='Java'])[1]")
+    public WebElement javaselected;
     //TC_03
-    @FindBy(xpath = "//button[.='Menu']")
-    public WebElement menuButton2;
-    @FindBy(xpath = "//a[.='Lesson Management']")
-    public WebElement lessonmanagement2;
     @FindBy(xpath = "(//option[.='Choose Education Term'])[1]")
     public WebElement chooseeducationterm;
+    @FindBy(xpath = "//option[.='FALL_SEMESTER']")
+    public WebElement semesterselected;
     @FindBy(xpath = "//option[.='Choose Day']")
     public WebElement chooseday;
+    @FindBy(xpath = "<option value=\"FRIDAY\">FRIDAY</option>")
+    public WebElement fridayselected;
     @FindBy(xpath = "//input[@id='startTime']")
     public WebElement starttime;
+    @FindBy(xpath = "//input[@value='21:57']")
+    public WebElement timevalue;
     @FindBy(xpath = "//input[@id='stopTime']")
     public WebElement stoptime;
+    @FindBy(xpath = "//input[@value='23:00']")
+    public WebElement stoptimevalue;
     @FindBy(xpath ="(//button[.='Submit'])[1]")
     public WebElement submitButtonlesprog;
 
--- src/test/resources/features/US_10_ViceDean_mertcan.feature
+++ src/test/resources/features/US_10_ViceDean_mertcan.feature
@@ -12,21 +12,21 @@ Feature: US_10 Vice Deans should be able to create a lesson schedule.
 
     @TC_02
     Scenario: Vice Dean should be able to select a course
-      When click menu button
+      When click lesson program
       And click choose lesson management button
-      And click lesson program
       And click choose lesson button
       Then choose lesson
 
+
      @TC_03
      Scenario: Vice Dean should be able to select a semester,day for the course and the start,end time
-       When click menu button2
-       And  click lesson management
+       When click 'choose education term'
        And  choose education term
        And  click 'choose day'
-       And  choose day
-       Then click start time
-       And  choose start time
+       And  choose choose day
+       Then choose day
+       And  click start time
+       Then choose start time
        And  click stop time
        Then choose stop time
        And  click submit button
